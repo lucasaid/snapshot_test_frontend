d35e365dcdc6440c18e2e0f5a31b2936320b5435
Chris Lucas <chris.lucas.dev@gmail.com>
2025-03-30 13:44:28 +1100

    Main entry

---
+++ src/main.tsx
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Container } from "@mui/material";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { StrictMode } from "react";
+import Snapshots from "./pages/Snapshots.tsx";
+import SnapshotsUpload from "./pages/SnapshotsUpload.tsx";
+
+import "./index.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+const queryClient = new QueryClient();
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <Container maxWidth="lg">
+        <Router>
+          <Routes>
+            <Route index element={<Snapshots />} />
+            <Route path="/upload" element={<SnapshotsUpload />} />
+            <Route path="/:id" element={<SnapshotsUpload />} />
+          </Routes>
+        </Router>
+      </Container>
+    </QueryClientProvider>
+  </StrictMode>
+);
